386d658526f47b183cb2fdac608a24204773e7d1
CuriosBasant <basantbrpl@gmail.com>
2024-10-28 11:42:27 +0530

    chore: 🔨 init `windows`

---
+++ windows/mediaplayer/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::mediaplayer::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::mediaplayer::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
